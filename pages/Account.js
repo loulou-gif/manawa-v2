acfbdeb1bb0e41b2470dcdfb47943cbc32dfd079
unknown <konankanjulius10@gmail.com>
2024-05-20 21:02:50 +0000

    date formulaire reservation

--- pages/Account.js
+++ pages/Account.js
@@ -1,15 +1,24 @@
-import { View, Text, Pressable, Image, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, Pressable, Image,Modal, StyleSheet, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 import  Header  from '../components/Header'
 // import SearchBar from '../components/SearchBar'
 import StoreHeader from '../components/StoreHeader'
 import { product } from '../data/Product'
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { TextInput } from 'react-native-paper'
+import IconeFontisto from 'react-native-vector-icons/Fontisto'
+import IconeIonicons from 'react-native-vector-icons/Ionicons'
 const Account = ({route, navigation}) => {
   const [date, setDate] = useState(new Date())
+  const [mode, setMode] = useState()
   const [showPicker, setShowPicker] = useState(false)
+  const [details, setDetails] = useState(false)
   const {id} = route.params;
   FilterData = product.filter((data) => data.id_store === id)
+
+  const handleVisible= () =>{
+    setDetails(!details)
+  }
 
   const onChange = (event, updateDate) =>{
     const currentDate = updateDate || date;
@@ -17,9 +26,18 @@ const Account = ({route, navigation}) => {
     setDate(currentDate)
   }
 
-  const showDataPicker =()=>{
+  const dateShower =()=>{
+    setMode('date')
     setShowPicker(true)
   }
+  const timeShower =()=>{
+    setMode('time')
+    setShowPicker(true)
+  }
+
+  // const showDataPicker =()=>{
+  //   setShowPicker(true)
+  // }
   return (
     <View>  
       {/* <Header/> */}
@@ -35,15 +53,43 @@ const Account = ({route, navigation}) => {
             </View>
             <View style={styles.space} >
               <Text  style={styles.price}>{data.price}</Text>
-              <Pressable style={styles.bouton} onPress={showDataPicker}>
+              <Pressable style={styles.bouton} onPress={handleVisible}>
                 <Text style={styles.value} >Réserver</Text>
               </Pressable>
             </View>
+            <Modal animationType='fade' transparent={true} visible={details}>
+               {details &&
+                  <View style={styles.background}>
+                    <View style={styles.box}>
+                        <Text style={styles.titre}>Réservation</Text>
+                        <View style={styles.all_input}>
+                          <View style={styles.input} ><Text>{details.name}</Text></View>
+                          <View style={styles.input} ><Text>{details.description}</Text></View>
+                          <View style={styles.input} ><Text>{details.price}</Text></View>
+                          <View style={styles.date}>  
+                            <Pressable style={styles.inputs} onPress={dateShower} ><Text style={styles.day}><IconeFontisto name='date' size={16} /> 11/11/2111</Text></Pressable>
+                            <Pressable style={styles.inputs} onPress={timeShower} ><Text style={styles.day}><IconeIonicons name='time-outline' size={16} style={{marginTop:50}}/> 11:11</Text></Pressable>
+                          </View>
+                          {/* <TextInput style={styles.input} value={date} onPressIn={showDataPicker}/> */}
+                          {/* <TextInput value={}/> */}
+                        </View>
+                        <View style={styles.bouttons}>
+                          <Pressable style={styles.btn_cancel} onPress={handleVisible}>
+                            <Text style={styles.text_btn}>Retour</Text>
+                          </Pressable >
+                          <Pressable style={styles.btn_valid} onPress={handleVisible}>
+                            <Text style={styles.text_btn}>Valider</Text>
+                          </Pressable>
+                        </View>
+                    </View>
+                  </View>
+                }
+            </Modal>
             {showPicker && (
               <DateTimePicker
                 testID="dateTimePicker"
                 value={date}
-                mode="date"
+                mode={mode}
                 is24Hour={true}
                 display="default"
                 onChange={onChange}
@@ -120,6 +166,82 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     height: 43,
    },
+   background:{
+    alignItems:'center',
+    justifyContent:'center',
+    height:'100%',
+    width:'100%',
+    backgroundColor:'rgba(0,0,0,0.2)',
+   },
+   box:{
+    width:'90%',
+    height:'40%',
+    backgroundColor:'white',
+    alignItems:'center'
+   },
+   titre:{
+    textAlign:'center',
+    fontSize:25,
+    marginTop:15
+   },
+   all_input:{
+    width:'80%',
+    marginTop:10,
+    marginBottom:10
+   },
+   date:{
+    flexDirection:'row',
+    width:'100%',
+    marginBottom:10,
+    justifyContent:'space-between',
+   },
+   input:{
+    borderWidth:1,
+    backgroundColor:'#fff',
+    height:40,
+    borderColor:"#D9D9D9",
+    marginTop:10,
+    borderRadius:10
+   },
+   inputs:{
+    borderWidth:1,
+    backgroundColor:'#fff',
+    height:40,
+    borderColor:"#D9D9D9",
+    marginTop:5,
+    borderRadius:10,
+    width:140,
+   },
+   bouttons:{
+    // borderWidth:1,
+    width:200,
+    flexDirection:'row',
+    justifyContent:'space-around',
+    height:'10%'
+   },
+   btn_cancel:{
+    // borderWidth:1,
+    width:'45%',
+    backgroundColor:'orange'    
+   },
+   btn_valid:{
+    // borderWidth:1,
+    width:'45%',
+    backgroundColor:'grey' ,   
+   },
+   text_btn:{
+    color:'white',
+    textAlign:'center',
+    alignContent:'center',
+    marginTop:5
+   },
+   day:{
+    alignContent:'center',
+    textAlign:'center',
+    marginTop:10,
+    color:'#ABA9A9',
+    width:'100%',
+   }
 })
 
 export default Account
