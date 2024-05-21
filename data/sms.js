3d3cbacf3e4dea43ea14cefb94c8394f2be1bdfb
unknown <konankanjulius10@gmail.com>
2024-05-21 22:18:41 +0000

    add sms data

---
+++ data/sms.js
@@ -0,0 +1,38 @@
+export const sms = [
+    {
+        id:1,
+        name:'Jared',
+        text:'Bonjours j\'espère que tu te portes bien',
+        pp:''
+    },
+    {
+        id:2,
+        name:'Julius',
+        text:'Bonjours j\'espère que tu te portes bien',
+        pp:''
+    },
+    {
+        id:3,
+        name:'Junias',
+        text:'Bonjours j\'espère que tu te portes bien',
+        pp:''
+    },
+    {
+        id:4,
+        name:'Jériel',
+        text:'Bonjours j\'espère que tu te portes bien',
+        pp:''
+    },
+    {
+        id:5,
+        name:'Annaelle',
+        text:'Bonjours j\'espère que tu te portes bien',
+        pp:''
+    },
+    {
+        id:6,
+        name:'Ruth',
+        text:'Bonjours j\'espère que tu te portes bien',
+        pp:''
+    },
+]
