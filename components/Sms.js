f0b5f50190bc01a9e6c346c168fde4681ffd3f37
unknown <konankanjulius10@gmail.com>
2024-05-22 14:40:45 +0000

    adjustment

--- components/Sms.js
+++ components/Sms.js
@@ -1,47 +1,68 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import {sms} from '../data/sms';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { sms } from '../data/sms';
+
+// Fonction pour tronquer le texte
+const truncateText = (text, length) => {
+  return text.length > length ? text.substring(0, length) + '...' : text;
+};
 
 const Sms = () => {
   return (
     <View>
-      {sms.map((d) =>(
+      {sms.map((d) => (
         <TouchableOpacity key={d.id} style={styles.card}>
-            <Image style={styles.image} source={d.pp} ></Image>
-            <View style={styles.info}>
-                <Text style={styles.name}>{d.name}</Text>
-                <Text style={styles.message}>{d.text}</Text>
-            </View>
-      </TouchableOpacity>
+          <Image style={styles.image} source={d.pp} />
+          <View style={styles.info}>
+            <Text style={styles.name}>{d.name}</Text>
+            <Text style={styles.message}>{truncateText(d.text, 50)}</Text>
+          </View>
+          <View style={styles.time_box}>
+            <Text style={styles.time}>{d.time}</Text>
+          </View>
+        </TouchableOpacity>
       ))}
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    card:{
-        borderColor:'#ABA9A9',
-        borderBottomWidth:1,
-        flexDirection:'row',
-        height:80,
-        alignItems:'center'
-    },
-    image:{
-        width:50,
-        height:50,
-        backgroundColor:'#DE9F42',
-        borderRadius:25,
-        marginLeft:20,
-        marginRight:20
-    },
-    name:{
-        fontSize:16,
-        color:'#47300D',
-    },
-    message:{
-        fontSize:14,
-        color:'#ABA9A9',
-    },
-})
+  card: {
+    borderColor: '#ABA9A9',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    height: 80,
+    alignItems: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#ABA9A9',
+    borderRadius: 25,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  name: {
+    fontSize: 16,
+    color: '#47300D',
+  },
+  message: {
+    fontSize: 14,
+    color: '#ABA9A9',
+  },
+  time_box: {
+    width: 80,
+    alignItems: 'center',
+    height: 40,
+    justifyContent: 'flex-end',
+  },
+  time: {
+    fontSize: 12,
+    color: '#ABA9A9',
+  },
+  info: {
+    flex: 1, // to take up the remaining space in the row
+  },
+});
 
-export default Sms
+export default Sms;
--- data/sms.js
+++ data/sms.js
@@ -2,37 +2,43 @@ export const sms = [
     {
         id:1,
         name:'Jared',
-        text:'Bonjours j\'espère que tu te portes bien',
-        pp:require('../assets/images/Profils/PP1.jpg')
+        text:'Bonjours j\'espère que tu te portes bien dfefgqdf qsfsdfdfw qdfqdsfdf qfqfsqdfgqsfqsfd',
+        pp:require('../assets/images/Profils/PP1.jpg'),
+        time:'Lundi'
     },
     {
         id:2,
         name:'Julius',
         text:'Bonjours j\'espère que tu te portes bien',
-        pp:require('../assets/images/Profils/PP2.jpg')
+        pp:require('../assets/images/Profils/PP2.jpg'),
+        time:'Hier'
     },
     {
         id:3,
         name:'Junias',
         text:'Bonjours j\'espère que tu te portes bien',
-        pp: require('../assets/images/Profils/PP3.jpg')
+        pp: require('../assets/images/Profils/PP3.jpg'),
+        time:'Mercredi'
     },
     {
         id:4,
         name:'Jériel',
         text:'Bonjours j\'espère que tu te portes bien',
-        pp:require('../assets/images/Profils/PP4.jpg')
+        pp:require('../assets/images/Profils/PP4.jpg'),
+        time:'09:15'
     },
     {
         id:5,
         name:'Annaelle',
         text:'Bonjours j\'espère que tu te portes bien',
-        pp:require('../assets/images/Profils/PP5.jpg')
+        pp:require('../assets/images/Profils/PP5.jpg'),
+        time:'Vendredi'
     },
     {
         id:6,
         name:'Ruth',
         text:'Bonjours j\'espère que tu te portes bien',
-        pp:require('../assets/images/Profils/PP6.jpg')
+        pp:require('../assets/images/Profils/PP6.jpg'),
+        time:'12/05/2021'
     },
 ]
